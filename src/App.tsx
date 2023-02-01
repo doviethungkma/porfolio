ba6fc7de86c961bf2a31f37fea910aa432c85e2b
doviethungkma <shadyside1995@gmail.com>
2023-02-01 13:47:19 +0700

    feature: App layout, navbar, sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import Sidebar from "./components/shared/Sidebar";
+import Main from "./components/shared/Main";
+import Navbar from "./components/shared/Navbar";
+import { useState } from "react";
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const [isShowSideBar, setIsShowSideBar] = useState<boolean>(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  const toggleSidebar = () => {
+    setIsShowSideBar(!isShowSideBar);
+  };
+
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello</h1>
+    <div className={`${isDarkMode && "dark"} app relative flex flex-col `}>
+      <Navbar toggleDarkMode={toggleDarkMode} toggleSideBar={toggleSidebar} />
+      {isShowSideBar && <Sidebar />}
+      <Main />
     </div>
   );
 }
