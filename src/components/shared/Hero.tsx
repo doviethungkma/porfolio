76159b7d935a457470b8f8b50947ad3bdc39148a
doviethungkma <shadyside1995@gmail.com>
2023-02-02 20:36:39 +0700

    Feature: About section

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -12,26 +12,28 @@ const typeSequences = [
 
 const Hero = () => {
   return (
-    <section className="max-w-[450px] lg:max-w-[830px] xl:h-screen flex flex-col lg:flex-row lg:gap-[80px] items-center gap-4 ">
-      <div className="w-[200px] h-[200px] rounded-avatar bg-avatar bg-center bg-cover shadow-avatar animate-avatar md:w-[300px] md:h-[300px]"></div>
-      <div className="flex flex-col items-center gap-4 lg:items-start">
-        <h1 className="text-black text-[35px] font-montserrat font-extrabold dark:text-white">
-          DO VIET HUNG
-        </h1>
-        <TypeAnimation
-          sequence={typeSequences}
-          wrapper="h2"
-          speed={40}
-          deletionSpeed={80}
-          cursor={true}
-          repeat={Infinity}
-          className="text-black text-[24px] font-montserrat dark:text-white"
-        />
-        <p className="text-center font-medium font-montserrat lg:text-left">
-          Creative Photographer based in New York and happy to travel all over
-          Europe to capture photos.
-        </p>
-        <Social />
+    <section className="w-full flex justify-center">
+      <div className="max-w-[450px] lg:max-w-[830px] min-h-screen xl:h-screen flex flex-col lg:flex-row lg:gap-[80px] items-center gap-4 ">
+        <div className="w-[200px] h-[200px] rounded-avatar bg-avatar bg-center bg-cover shadow-avatar animate-avatar md:w-[300px] md:h-[300px]"></div>
+        <div className="flex flex-col items-center gap-4 lg:items-start">
+          <h1 className="text-black text-[35px] font-montserrat font-extrabold dark:text-white">
+            DO VIET HUNG
+          </h1>
+          <TypeAnimation
+            sequence={typeSequences}
+            wrapper="h2"
+            speed={40}
+            deletionSpeed={80}
+            cursor={true}
+            repeat={Infinity}
+            className="text-black text-[24px] font-montserrat dark:text-white"
+          />
+          <p className="text-center font-medium font-montserrat lg:text-left">
+            Creative Photographer based in New York and happy to travel all over
+            Europe to capture photos.
+          </p>
+          <Social />
+        </div>
       </div>
     </section>
   );
---
+++ src/data/shortInfor.tsx
@@ -0,0 +1,37 @@
+export const shortInfor = [
+  {
+    title: "birthday",
+    value: "18/05/1995",
+  },
+  {
+    title: "address",
+    value: "Tong Lanh - Thuan Chau - Son La",
+  },
+  {
+    title: "Email",
+    value: "doviethung1995@gmail.com",
+  },
+  {
+    title: "phone",
+    value: "+84 0352773054",
+  },
+];
+
+export const studyInfor = [
+  {
+    title: "Study",
+    value: "Academy of cryptography techniques",
+  },
+  {
+    title: "degree",
+    value: "Engineer",
+  },
+  {
+    title: "classification",
+    value: "Good",
+  },
+  {
+    title: "score",
+    value: "2.71 / 4",
+  },
+];
